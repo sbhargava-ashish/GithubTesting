a44775afd1888c4c6eda38c5e1f79205e849fe5b
Ashish <sbhargava.ashish@gmail.com>
2017-12-02 21:09:00 +0530

    Test for changes in the remote repository

--- GitRepositoryTest/GitRepositiryTest/src/com/ashish/git/GitRepositiryTest.java
+++ GitRepositoryTest/GitRepositiryTest/src/com/ashish/git/GitRepositiryTest.java
@@ -3,7 +3,7 @@ package com.ashish.git;
 public class GitRepositiryTest {
 
 	public static void main(String[] args) {
-		System.out.println("This is Github test for remote checkin ");
+		System.out.println("This is Github test for remote checkin new one ");
 	}
 
 }
